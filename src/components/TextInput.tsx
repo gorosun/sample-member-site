4c7f65aea2c14aecae90ddfdfba015dd45a7b77b
gorosun <goro-hayakawa@no-studio.net>
2024-09-11 16:58:53 +0900

    Set up React development and testing environment.

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,22 @@
+import React, { FC, memo, useState } from 'react';
+
+const TextInput: FC = memo(() => {
+  const [text, setText] = useState('');
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        aria-label="Text Input"
+      />
+      <p>Entered Text: {text}</p>
+    </div>
+  );
+});
+
+TextInput.displayName = 'TextInput'; // Display nameを明示的に設定
+export { TextInput };
